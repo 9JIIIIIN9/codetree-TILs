12b8a750823b5c5c6ce7353af5078a2bc95911fb
9JIIIIIN9 <74411978+9JIIIIIN9@users.noreply.github.com>
2024-08-13 22:40:17 +0900

    Title: 순서대로 구구단, Time: 170ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240813/순서대로 구구단/multiplication-table-in-order.js
+++ 240813/순서대로 구구단/multiplication-table-in-order.js
@@ -2,8 +2,12 @@ var fs = require('fs');
 var input = fs.readFileSync('/dev/stdin').toString().split(' ');
 var a = parseInt(input[0]);
 var b = parseInt(input[1]);
-for(let i=1;i<10;i++){
-    process.stdout.write(a+' * '+i+' = '+(a*i)+'  ');
-    process.stdout.write(b+' * '+i+' = ' +(b*i)+ '\n');
+    for(let i=1;i<10;i++){
+        for(let j=a;j<b+1;j++){
+
+        process.stdout.write(j+' * '+i+' = '+(j*i)+'  ');
+
+    }
+    console.log();
 
 }
